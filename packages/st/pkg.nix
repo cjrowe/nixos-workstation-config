4df785fef53efe6d9b2452fd872a8b0f5313c3db
Chris Rowe <chris@chrisrowe.co.uk>
2023-08-23 16:23:29 +0100

    Added harfbuzz as input

--- packages/st/pkg.nix
+++ packages/st/pkg.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, libX11, libXinerama, libXft, fontconfig, pkg-config, ncurses }:
+{ stdenv, pkgs, libX11, libXinerama, libXft, fontconfig, pkg-config, ncurses, harfbuzz }:
 with pkgs.lib;
 
 stdenv.mkDerivation rec {
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
   src = ./source;
 
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ libX11 libXft fontconfig ncurses ];
+  buildInputs = [ libX11 libXft fontconfig ncurses harfbuzz ];
 
 
   unpackPhase = ''cp -r $src/* .'';
